d0e667bf1f183529e05cd3e841c3ef22a3ba892a
Luiza Campos <luiza.almcampos@gmail.com>
2021-02-08 15:12:55 -0300

    shadow DOM style studies

--- tooltip.js
+++ tooltip.js
@@ -7,14 +7,45 @@ class Tooltip extends HTMLElement {
    this.shadowRoot.innerHTML = `
     <style>
       div {
+        font-weight: normal;
         background-color: black;
         color: white;
         position: absolute;
+        top: 1.5rem;
+        left: 0.75rem;
         z-index: 10;
+        padding: 0.12rem;
+        border-radius: 3px;
+        box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
+      }
+
+      :host(.important) {
+        background-color: var(--color-primary, #ccc);
+        padding: 0.15rem;
+      }
+
+      :host-context(p) {
+        font-weight: bold;
+      }
+
+      .highlight {
+        background-color: red;
+      }
+
+      ::slotted(.highlight) {
+        border-bottom: 3px solid blue;
+      }
+
+      .icon {
+        background-color: black;
+        color: white;
+        padding: 0.15rem 0.5rem;
+        text-align: center;
+        border-radius: 50%;     
       }
     </style>
     <slot>Some default</slot>
-    <span> (?)</span>
+    <span class="icon">?</span>
   `;
   }
 
